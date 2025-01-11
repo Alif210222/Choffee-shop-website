36a09413fd40bb51446a4dfb3732ca60461f8f57
Alif-Sarker <sarker202454@gmil.com>
2025-01-11 23:51:56 +0600

    all code pushed & first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const links = document.querySelectorAll('#mainmenu ul li a');
+
+links.forEach(link =>{
+    link.addEventListener('click' ,function(event){
+        links.forEach(link=>{
+            link.parentElement.classList.remove('active')
+        });
+        this.parentElement.classList.add('active');
+    });
+});
+
+
+const navDialog = document.getElementById('nav-dialog');
+function handleMenu(){
+ navDialog.classList.toggle('hidden');
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily:{
+        poppins:["poppins","sens-serif"],
+      },
+      width:{
+        "custom-image-width":"100px"
+      }
+    },
+  },
+  plugins: [],
+}
+
